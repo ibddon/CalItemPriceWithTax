c67f77f1107f45fa43bfc325c8552d8a8bcf0ce7
ibddon <ibrahim.igee@yahoo.com>
2018-05-22 20:55:33 -0700

    New commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var price = require('./price.js');
+
+var result = price.totalWithPrice(5.99, 0.09, function(status, msg){
+
+    console.log(status);
+    console.log(msg)
+
+});
+
+var res = console.log(price.totalWithPrice2(5.99, 0.09));
+
---
+++ price.js
@@ -0,0 +1,37 @@
+checkPrice = {};
+
+
+var isNumber = function (num){
+    return  typeof num === "number" && num > 0;
+
+};
+
+checkPrice.totalWithPrice = function (potentialPrice , taxRate, callBack) {
+
+    if (typeof callBack !== "function") return callBack(false, 'Error');
+
+    var res1 = isNumber(potentialPrice, taxRate);
+
+    var res2  = (potentialPrice * taxRate) + potentialPrice;
+
+    if (res1 && res2)  {
+
+        callBack(true, null)
+
+    } else {
+
+        callBack(false, 'error, enter a valid whole number')
+    }
+};
+
+checkPrice.totalWithPrice2 = function (potentialPrice , taxRate) {
+
+    var res1 = isNumber(potentialPrice, taxRate);
+
+    var res2  = (potentialPrice * taxRate) + potentialPrice;
+
+    return res1 && res2;
+};
+         
+
+module.exports = checkPrice;
